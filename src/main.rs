e2ae8f225cf55700afc59b224840f87eaf8910d7
santiago-lambda <santiago.pittella@lambdaclass.com>
2022-06-14 18:21:30 -0300

    finally, persistency

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::collections::HashMap;
-use std::fmt::Display;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use axum::{
   extract::{Json, Extension, Path},
   response::IntoResponse,
-  http::StatusCode,
   routing::{get, post},
   Router,
 };
@@ -13,15 +11,15 @@ use axum::{
 use serde::{Deserialize, Serialize};
 
 // Structs
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Serialize, Default)]
 struct State {
   users: HashMap<u8, User>,
   items: HashMap<u8, Item>,
 }
 
 impl State {
-  fn new() -> Self {
-    State { users: HashMap::new(), items: HashMap::new()}
+  fn new(mut users_hashmap: HashMap<u8, User>, mut items_hashmap: HashMap<u8, Item>) -> Self {
+    State { users: users_hashmap, items: items_hashmap}
   }
 }
 
@@ -51,13 +49,15 @@ impl Item {
   }
 }
 
+type SharedState = Arc<Mutex<State>>;
+
 #[tokio::main]
 async fn main() {
   tracing_subscriber::fmt::init();
 
   // not the greatest idea, but i will save items and users in this hash maps
-  let mut state = State::new();
-  // let mut state: Arc<State> = Arc::new(State::new());
+  // let mut state = State::new();
+  let mut state: SharedState = Arc::new(Mutex::new(State::new(HashMap::new(), HashMap::new())));
 
   // build our application with a single route
   let app = Router::new()
@@ -77,28 +77,26 @@ async fn main() {
 }
 
 // Handlers
-async fn get_state(Extension(state): Extension<State>) -> impl IntoResponse {
-  Json(state)
+async fn get_state(Extension(state): Extension<SharedState>) -> impl IntoResponse {
+  // Json(&state)
 }
 
-async fn get_user(Path(user_id): Path<u8>, Extension(state): Extension<State>) -> impl IntoResponse {
-  print!("{:?}", state);
-  let user: &User = state.users.get(&user_id).unwrap();
-  Json(user.clone())
+async fn get_user(Path(user_id): Path<u8>, Extension(state): Extension<SharedState>) -> impl IntoResponse {
+  let db = &state.lock().unwrap().users;
+  Json(db.get(&user_id).unwrap().clone())
 }
-async fn create_user(Json(user_rq): Json<User>, Extension(mut state): Extension<State>) -> impl IntoResponse {
+async fn create_user(Json(user_rq): Json<User>, Extension(state): Extension<SharedState>) -> impl IntoResponse {
   let user: User =  User::new(user_rq.username.to_string(), user_rq.age, user_rq.id);
-  state.users.insert(user_rq.id, user.clone());
+  &mut state.lock().unwrap().users.insert(user_rq.id, user.clone());
   Json(user)
 }
 
-async fn get_item(Path(item_id): Path<u8>, Extension(state): Extension<State>) -> impl IntoResponse {
-  let item: &Item = state.items.get(&item_id).unwrap();
-  Json(item.clone())
+async fn get_item(Path(item_id): Path<u8>, Extension(state): Extension<SharedState>) -> impl IntoResponse {
+  let db = &state.lock().unwrap().items;
+  Json(db.get(&item_id).unwrap().clone())
 }
-async fn create_item(Json(item_rq): Json<Item>, Extension(mut state): Extension<State>) -> impl IntoResponse {
+async fn create_item(Json(item_rq): Json<Item>, Extension(mut state): Extension<SharedState>) -> impl IntoResponse {
   let item: Item = Item::new(item_rq.name.to_string(), User::new(item_rq.owner.username.to_string(), item_rq.owner.age, item_rq.owner.id), item_rq.id);
-  print!("{:?}", item);
-  let item: Item = state.items.insert(item_rq.id, item).unwrap();
+  &mut state.lock().unwrap().items.insert(item_rq.id, item.clone());
   Json(item)
 }
